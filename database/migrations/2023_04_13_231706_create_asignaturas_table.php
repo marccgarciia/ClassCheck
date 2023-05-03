25d62180862c85a5dc2fea8f8789587f50d3eabe
Edgar Marcos <fifacallput2002@gmail.com>
2023-05-03 17:31:05 +0200

    Last update

--- database/migrations/2023_04_13_231706_create_asignaturas_table.php
+++ database/migrations/2023_04_13_231706_create_asignaturas_table.php
@@ -9,18 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('asignaturas', function (Blueprint $table) {
-            $table->id();
-            $table->string("nombre");
-            $table->unsignedBigInteger('id_curso');
-            $table->unsignedBigInteger('id_profesor');
+public function up(): void
+{
+    Schema::create('asignaturas', function (Blueprint $table) {
+        $table->id();
+        $table->string("nombre");
+        $table->unsignedBigInteger('id_curso');
+        $table->unsignedBigInteger('id_profesor');
+        $table->date('fecha_inicio');
+        $table->date('fecha_fin');
 
-            $table->foreign('id_profesor')->references('id')->on('profesores')->onDelete('cascade');
-            $table->foreign('id_curso')->references('id')->on('cursos')->onDelete('cascade');
-        });
-    }
+        $table->foreign('id_profesor')->references('id')->on('profesores')->onDelete('cascade');
+        $table->foreign('id_curso')->references('id')->on('cursos')->onDelete('cascade');
+    });
+}
+
 
     /**
      * Reverse the migrations.
