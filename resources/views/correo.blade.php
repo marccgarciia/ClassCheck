870ac721907c3cc4bd9fba9830d5ac4919d28546
DaniGarcia09 <contreras.garcia.dani@gmail.com>
2023-05-05 17:28:26 +0200

    last

--- resources/views/correo.blade.php
+++ resources/views/correo.blade.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="en">
-=======
 <html lang="es">
->>>>>>> 2b383a96b644d5ab3e7fb23becbe455c8b346b3d
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,10 +9,6 @@
 <body>
     <h1>{{$datos['bnv']}}<h1>
     <h3>{{$datos['msg']}}<h3>
-<<<<<<< HEAD
-    <img src="https://lh3.googleusercontent.com/d/1ZygU-aL2X_nGodzbwuniJrTN_bM7FUKL">    
-=======
         <img src="https://lh3.googleusercontent.com/d/1ZygU-aL2X_nGodzbwuniJrTN_bM7FUKL" alt="Logo Classcheck" width="200px">
->>>>>>> 2b383a96b644d5ab3e7fb23becbe455c8b346b3d
 </body>
 </html>
