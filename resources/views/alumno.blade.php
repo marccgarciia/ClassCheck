05b213b144cbc43807aa3947c73ac1ec53f9fdb1
VictorMalaret <71766386+VictorMalaretGarciolo@users.noreply.github.com>
2023-05-05 17:31:38 +0200

    last update

--- resources/views/alumno.blade.php
+++ resources/views/alumno.blade.php
@@ -27,16 +27,38 @@
     <h1>AQUI VA LA INTEERFAZ DEL CALENDARIO</h1>
 
 
-    <form action="{{ route('passalumno.panel') }}" method="POST">
+    <form id="password-form" action="{{ route('passalumno.panel') }}" method="POST">
         @csrf
     
         <div>
             <label for="newpass">Nueva contraseña</label>
             <input type="password" id="newpass" name="newpass" required>
         </div>
+
+        <div>
+            <label for="confirmpass">Confirmar contraseña</label>
+            <input type="password" id="confirmpass" name="confirmpass" required>
+        </div>    
     
         <button type="submit">Cambiar contraseña</button>
     </form>
+
+    <script>
+        const form = document.getElementById('password-form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault(); // Evita que el formulario se envíe automáticamente
+    
+            const newPass = document.getElementById('newpass').value;
+            const confirmPass = document.getElementById('confirmpass').value;
+    
+            if (newPass !== confirmPass) {
+                alert('La confirmación de contraseña no coincide');
+                return;
+            }
+    
+            form.submit(); // Si la validación es correcta, envía el formulario
+        });
+    </script>
 
 
 @endsection
--- resources/views/profesor.blade.php
+++ resources/views/profesor.blade.php
@@ -6,7 +6,7 @@
 
     <h1>AQUI VA LA INTEERFAZ DEL CALENDARIO CON FILTROS Y CRUD DE SUS ALUMNOS EN SU CLASE</h1>
 
-    <form action="{{ route('passprofe.panel') }}" method="POST">
+    <form id="password-form" action="{{ route('passprofe.panel') }}" method="POST">
         @csrf
 
         <div>
@@ -14,7 +14,29 @@
             <input type="password" id="newpass" name="newpass" required>
         </div>
 
+        <div>
+            <label for="confirmpass">Confirmar contraseña</label>
+            <input type="password" id="confirmpass" name="confirmpass" required>
+        </div>    
+
         <button type="submit">Cambiar contraseña</button>
     </form>
+
+    <script>
+        const form = document.getElementById('password-form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault(); // Evita que el formulario se envíe automáticamente
+    
+            const newPass = document.getElementById('newpass').value;
+            const confirmPass = document.getElementById('confirmpass').value;
+    
+            if (newPass !== confirmPass) {
+                alert('La confirmación de contraseña no coincide');
+                return;
+            }
+    
+            form.submit(); // Si la validación es correcta, envía el formulario
+        });
+    </script>
 
 @endsection
