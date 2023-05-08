041b90dfff2f06c5d0a89fe0cab4d0b4d3c731a6
Marc García-Cuevas de Paz <marcg-c20@hotmail.es>
2023-05-08 17:23:08 +0200

    Styles Datos

--- resources/views/datosprof.blade.php
+++ resources/views/datosprof.blade.php
@@ -1,37 +1,18 @@
-    <!-- Main -->
-    <div class="main">
-        <h2>Identidad</h2>
-        <div class="card">
-            <div class="card-body">
-                <i class="fa fa-pen fa-xs edit"></i>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Nombre</td>
-                            <td>:</td>
-                            <td>Gerard</td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td>:</td>
-                            <td>profesortest@gmail.com</td>
-                        </tr>
-                        <tr>
-                            <td>Dirección</td>
-                            <td>:</td>
-                            <td>Hospitalet, Barcelona</td>
-                        </tr>
-                    </tbody>
-                </table>
-                    <form action="{{ route('passalumno.panel') }}" method="POST">
-                        @csrf
-                    
-                        <div>
-                            <label for="newpass">Nueva contraseña</label>
-                            <input type="password" id="newpass" name="newpass" required>
-                        </div>
-                    
-                        <button type="submit">Cambiar contraseña</button>
-                    </form>
-            </div>
-        </div>
+<div class="divdatos">
+    <h1><b>Nombre:</b> Pepe</h1>
+    <h1><b>Correo Electrónico:</b> Pepe</h1>
+    <h1><b>Dirección:</b> Pepe</h1>
+    <h1><b>Curso:</b> Pepe</h1>
+    <br>
+    <h1 id="tituloscan">¿Quieres cambiar la contraseña?</h1>
+
+    <form action="{{ route('passalumno.panel') }}" method="POST">
+        @csrf
+
+        <label for="newpass">Nueva contraseña: </label>
+        <input type="text" id="newpass" name="newpass" required>
+
+        <button type="submit" class="btndatos">Cambiar</button>
+    </form>
+
+</div>
