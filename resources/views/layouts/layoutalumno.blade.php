9c5187cb9eb9dc7c4d4003dd3e29fdebc090b5be
Marc García-Cuevas de Paz <marcg-c20@hotmail.es>
2023-05-23 15:20:39 +0200

    CARGANDO OK

--- resources/views/layouts/layoutalumno.blade.php
+++ resources/views/layouts/layoutalumno.blade.php
@@ -120,63 +120,86 @@
             <!-- Contenedor de la pantalla de carga -->
             <div id="loader" style="display: none;">
                 <style>
+                    :root {
+                        --color-negro: #000;
+                        --color-azuloscuro: #142c44;
+                        --color-azulclaro: #2b4d6d;
+                        --color-azulmasclaro: #85b7e5;
+                        --color-gris: #eee;
+                        --color-blanco: #ffff;
+                        --color-input: #ffff;
+                        --color-texto-input: #142c44;
+                        --color-texto: #142c44;
+                        --rojo: #DB504A;
+                        --fc-border-color: #eeeeee;
+                    }
+
+                    body.dark {
+                        --color-azulclaro: #0C0C1E;
+                        --color-gris: #142c44;
+                        --color-blanco: #FBFBFB;
+                        --color-texto: #FBFBFB;
+                        --color-input: #2b4d6d;
+                        --color-texto-input: #85b7e5;
+                    }
+
                     .loaderB {
-                    background-color: #142c44;
-                    margin: 0;
-                    padding: 0;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    height: 100vh;
+                        background-color: #eeeeee;
+                        margin: 0;
+                        padding: 0;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        height: 100vh;
                     }
 
                     .loader {
-                    position: relative;
-                    width: 120px;
-                    height: 120px;
+                        position: relative;
+                        width: 120px;
+                        height: 120px;
                     }
 
                     .circle {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    bottom: 0;
-                    margin: auto;
-                    border: 7px solid #f3f3f3;
-                    border-top: 7px solid #2b4d6d;
-                    border-radius: 50%;
-                    width: 90px;
-                    height: 90px;
-                    animation: spin 2s linear infinite;
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        right: 0;
+                        bottom: 0;
+                        margin: auto;
+                        border: 7px solid #2b4d6d5e;
+                        border-top: 7px solid #2b4d6d;
+                        border-radius: 50%;
+                        width: 90px;
+                        height: 90px;
+                        animation: spin 2s linear infinite;
                     }
 
                     @keyframes spin {
-                    0% {
-                        transform: rotate(0deg);
-                    }
+                        0% {
+                            transform: rotate(0deg);
+                        }
 
-                    100% {
-                        transform: rotate(360deg);
-                    }
+                        100% {
+                            transform: rotate(360deg);
+                        }
                     }
 
                     .check-icon {
-                    position: absolute;
-                    top: 50%;
-                    left: 50%;
-                    transform: translate(-50%, -50%);
-                    color: white;
-                    font-size: 48px;
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        color: var(--color-azuloscuro);
+                        font-size: 48px;
                     }
                 </style>
 
                 <div class="loaderB">
-                <div class="loader">
-                    <div class="circle"></div>
-                    <i class="fas fa-check check-icon"></i>
+                    <div class="loader">
+                        <div class="circle"></div>
+                        <i class="fas fa-check check-icon"></i>
+                    </div>
                 </div>
-            </div>
             </div>
 
             <div id="contenedor-contenido">
@@ -197,18 +220,18 @@
 {{-- SCRIPT MODO OSCURO --}}
 {{-- ::::::::::::::::::::::::::::::::::::::::::::: --}}
 <script>
-    // $(document).ajaxStart(function() {
-    //     $('#loader').fadeIn(); // Mostrar la pantalla de carga al iniciar una solicitud AJAX
-    //     var body = document.body;
-    //     body.style.backgroundColor = '#142c44';
-    // });
+    $(document).ajaxStart(function() {
+        $('#loader').fadeIn(); // Mostrar la pantalla de carga al iniciar una solicitud AJAX
+        var body = document.body;
+        body.style.backgroundColor = '#eeeeee';
+    });
 
-    // $(document).ajaxStop(function() {
-    //     $('#loader').fadeOut(function() {
-    //     // Restablecer el color original del body después de ocultar la pantalla de carga
-    //     document.body.style.backgroundColor = '';
-    // });
-    // });
+    $(document).ajaxStop(function() {
+        $('#loader').fadeOut(function() {
+            // Restablecer el color original del body después de ocultar la pantalla de carga
+            document.body.style.backgroundColor = '';
+        });
+    });
     const sidemenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
     sidemenu.forEach(item => {
